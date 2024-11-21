8a0397bd741a479322a8e8ca667e4b2139286a63
IsaiahPressman <isaiahpressman16@gmail.com>
2024-11-21 17:41:23 -0500

    correctly clip to edge of board

--- src/luxai_s3/env.py
+++ src/luxai_s3/env.py
@@ -667,7 +667,10 @@ class LuxAIS3Env(environment.Environment):
         new_energy_nodes = jnp.clip(
             state.energy_nodes + energy_node_deltas,
             min=jnp.array([0, 0], dtype=jnp.int16),
-            max=jnp.array([self.fixed_env_params.map_width, self.fixed_env_params.map_height], dtype=jnp.int16),
+            max=jnp.array(
+                [self.fixed_env_params.map_width - 1, self.fixed_env_params.map_height - 1],
+                dtype=jnp.int16
+            ),
         )
         new_energy_nodes = jnp.where(
             state.steps * params.energy_node_drift_speed % 1 == 0,
