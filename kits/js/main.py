54a2ff5d6b22717b772854e536b870fdf4c162b7
Zhu Liang <zhuliangg11@gmail.com>
2024-12-10 10:54:25 +0800

    feat: fix js agent

--- kits/js/main.py
+++ kits/js/main.py
@@ -1,14 +1,14 @@
 import json
 from subprocess import Popen, PIPE
-from threading import Thread
+from threading  import Thread
 from queue import Queue, Empty
 from collections import defaultdict
+from argparse import Namespace
 import atexit
 import os
-import sys
-from argparse import Namespace
 
-agent_processes = defaultdict(lambda: None)
+### Don't change below! ###
+agent_processes = defaultdict(lambda : None)
 t = None
 q_stderr = None
 q_stdout = None
@@ -19,47 +19,91 @@ def cleanup_process():
         proc = agent_processes[agent_key]
         if proc is not None:
             proc.kill()
-
 def enqueue_output(out, queue):
     for line in iter(out.readline, b''):
         queue.put(line)
     out.close()
 
+import numpy as np
+game_state = None
+def from_json(state):
+    if isinstance(state, list):
+        return np.array(state)
+    elif isinstance(state, dict):
+        out = {}
+        for k in state:
+            out[k] = from_json(state[k])
+        return out
+    else:
+        return state 
+def process_obs(game_state, step, obs):
+    if step == 0:
+        # at step 0 we get the entire map information
+        game_state = from_json(obs)
+    else:
+        game_state = from_json(obs)
+    return game_state
+def to_json(obj):
+    if isinstance(obj, np.ndarray):
+        return obj.tolist()
+    elif isinstance(obj, np.integer):
+        return int(obj)
+    elif isinstance(obj, np.floating):
+        return float(obj)
+    elif isinstance(obj, list) or isinstance(obj, tuple):
+        return [to_json(s) for s in obj]
+    elif isinstance(obj, dict):
+        out = {}
+        for k in obj:
+            out[k] = to_json(obj[k])
+        return out
+    else:
+        return obj
 def agent(observation, configuration):
-    global agent_processes, t, q_stderr, q_stdout
-
+    """
+    a wrapper around a non-python agent
+    """
+    global agent_processes, t, q_stderr, q_stdout, game_state
+    import sys
+    if "__raw_path__" in configuration:
+        cwd = os.path.dirname(configuration["__raw_path__"])
+    else:
+        cwd = os.path.dirname(__file__)
+        if cwd == "":
+            cwd ="./"
     agent_process = agent_processes[observation.player]
+    ### Do not edit ###
     if agent_process is None:
-        if "__raw_path__" in configuration:
-            cwd = os.path.dirname(configuration["__raw_path__"])
-        else:
-            cwd = os.path.dirname(__file__)
-            if cwd == "":
-                cwd ="./"
         agent_process = Popen(["node", "main.js"], stdin=PIPE, stdout=PIPE, stderr=PIPE, cwd=cwd)
         agent_processes[observation.player] = agent_process
         atexit.register(cleanup_process)
 
+        # following 4 lines from https://stackoverflow.com/questions/375427/a-non-blocking-read-on-a-subprocess-pipe-in-python
         q_stderr = Queue()
         t = Thread(target=enqueue_output, args=(agent_process.stderr, q_stderr))
-        t.daemon = True
+        t.daemon = True # thread dies with the program
         t.start()
-    data = json.dumps(dict(obs=json.loads(observation.obs), step=observation.step, remainingOverageTime=observation.remainingOverageTime, player=observation.player, info=configuration))
+
+    json_obs = json.loads(observation.obs)
+    game_state = process_obs(game_state, observation.step, json_obs)
+    obs_inputs = to_json(game_state)
+
+    data = json.dumps(dict(obs=obs_inputs, step=observation.step, remainingOverageTime=observation.remainingOverageTime, player=observation.player, info=configuration))
     agent_process.stdin.write(f"{data}\n".encode())
     agent_process.stdin.flush()
 
-    agent_res = (agent_process.stdout.readline()).decode()
+    agent1res = (agent_process.stdout.readline()).decode()
     while True:
-        try:
-            line = q_stderr.get_nowait()
+        try:  line = q_stderr.get_nowait()
         except Empty:
+            # no standard error received, break
             break
         else:
+            # standard error output received, print it out
             print(line.decode(), file=sys.stderr, end='')
-    if agent_res == "":
+    if agent1res == "":
         return {}
-    return json.loads(agent_res)
-
+    return json.loads(agent1res)
 
 if __name__ == "__main__":
     
