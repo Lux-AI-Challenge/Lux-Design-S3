67f245e2206e09b156accb2f63fb4a23d4c916ce
stone_tao <stonezt2019@gmail.com>
2024-12-30 12:55:50 -0800

    adding missing build to gitignore and bump version

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="luxai-s3",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(exclude="kits"),
     install_requires=[
         "jax",
