a0ed0d2c1040550ef342a04799019a5f281fb463
stone_tao <stonezt2019@gmail.com>
2025-01-21 10:20:25 -0800

    version bump

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="luxai-s3",
-    version="0.1.1",
+    version="0.2.0",
     packages=find_packages(exclude="kits"),
     install_requires=[
         "jax",
