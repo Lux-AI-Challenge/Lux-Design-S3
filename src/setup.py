8188ad5cbb1fe1e6f6a8466ab5d703b38a1e0426
stone_tao <stonezt2019@gmail.com>
2025-01-24 13:18:59 -0800

    version bump and fix specs typo

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="luxai-s3",
-    version="0.2.0",
+    version="0.2.1",
     packages=find_packages(exclude="kits"),
     install_requires=[
         "jax",
